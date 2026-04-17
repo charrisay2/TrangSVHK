dbd8d4753615ee0bf3ab9291108746b63b85012a
Phu Quoc <nguyenphuquoc889@gmail.com>
2026-04-17 16:48:53 +0700

    rq

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,12 +1,11 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: '/api',
+  baseURL: import.meta.env.VITE_API_URL,
   headers: {
     'Content-Type': 'application/json',
   },
 });
-
 // Add a request interceptor to attach the token
 api.interceptors.request.use(
   (config) => {
